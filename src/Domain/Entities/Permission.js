4500b09d13c734fd201274987646b0f073cb0728
softyAmir <theyabcpssouaf2@gmail.com>
2025-03-19 23:40:57 -1100

    delete permission js

--- src/Domain/Entities/Permission.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const permissionSchema = new mongoose.Schema({
-  permissionName: { type: String, required: true, unique: true },
-});
-
-const Permission = mongoose.model("Permission", permissionSchema);
-
-module.exports = Permission;
